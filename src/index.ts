e0c33fa32b47a23b2dd7d1c494e0597f1f4f5205
DESKTOP-2ACJTHO\Fresher <magicghost.vu@gmail.com>
2018-05-18 18:06:24 +0700

    #add init file

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/**
+ * Created by Fresher on 18/05/2018.
+ */
+
+console.log("This is log from ts");
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Fresher on 18/05/2018.
+ */
+import path from 'path';
+let config = {
+    entry: './src/index.ts',
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.tsx', '.ts', '.js']
+    },
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
+};
+module.exports = config;
